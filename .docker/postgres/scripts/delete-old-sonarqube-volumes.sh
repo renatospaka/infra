5a0e24eb75b42a8bb6bbc6dbcb40c5c0e7f85c38
Renato Spakauskas <renatospaka@gmail.com>
2025-08-12 13:27:40 -0300

    build: :bug: fixed volumes' names by adding the prefix cloud_

--- .docker/postgres/scripts/delete-old-sonarqube-volumes.sh
+++ .docker/postgres/scripts/delete-old-sonarqube-volumes.sh
@@ -1,8 +1,8 @@
 
 #!/bin/bash
 
-docker volume ls | grep -E 'infra_postgres_data|infra_sonarqube_data|infra_sonarqube_extensions|infra_sonarqube_logs'
+docker volume ls | grep -E 'cloud_infra_postgres_data|cloud_infra_sonarqube_data|cloud_infra_sonarqube_extensions|cloud_infra_sonarqube_logs'
 
-docker volume rm infra_postgres_data infra_sonarqube_data infra_sonarqube_extensions infra_sonarqube_logs
+docker volume rm cloud_infra_postgres_data cloud_infra_sonarqube_data cloud_infra_sonarqube_extensions cloud_infra_sonarqube_logs
 
-docker volume ls | grep -E 'infra_postgres_data|infra_sonarqube_data|infra_sonarqube_extensions|infra_sonarqube_logs'
+docker volume ls | grep -E 'cloud_infra_postgres_data|cloud_infra_sonarqube_data|cloud_infra_sonarqube_extensions|cloud_infra_sonarqube_logs'
